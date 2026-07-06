59e66330dc5d980be79f038d9181410662060591
Guillermo Fernández <gafernandez2525@gmail.com>
2026-07-06 20:24:00 +0200

    Adjust format

--- app/Http/Controllers/SearchEventsController.php
+++ app/Http/Controllers/SearchEventsController.php
@@ -11,7 +11,6 @@ use Illuminate\Http\JsonResponse;
 class SearchEventsController extends Controller
 {
     public function index(IndexRequest $request): JsonResponse
-
     {
         $validated = $request->validated();
 
